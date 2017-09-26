d68531c30f0d6352db1bafc4e2d2221460be0866
kojiro540 <kojiro540@gmail.com>
2017-09-26 11:44:08 +0000

    getLatest data funciton

---
+++ getLatest.py
@@ -0,0 +1,20 @@
+import difflib
+
+def show(new):
+	print new
+
+def getLatest(func, oldFile, newFile):
+	d = difflib.Differ()
+	with open(oldFile, 'r') as f1, open(newFile, 'r') as f2:
+		differ = difflib.Differ()
+		new = []
+		for line in differ.compare(f1.read().splitlines() ,f2.read().splitlines()):
+			if line.startswith('+ '):
+				new.append(line[2:])
+	func(new)
+
+def main():
+	getLatest(show,'Test.txt.old','Test.txt')
+
+if __name__ == '__main__':
+	main()

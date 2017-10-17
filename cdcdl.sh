ad003d196f327545323b87b9dc78c1cd57d3ee81
EC2 Default User <ec2-user@ip-172-31-18-158.ap-southeast-1.compute.internal>
2017-10-17 18:15:56 +0000

    Fix cdcdl.sh download dir / .gitignore to include .pyc

--- cdcdl.sh
+++ cdcdl.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync --password-file=rsync_pass -avz rsync://darthvader@176.32.89.130/msd/SET/ ~/metastock-data/SET/
+rsync --password-file=rsync_pass -avz rsync://darthvader@176.32.89.130/msd/SET/ ~/ms-data/SET/

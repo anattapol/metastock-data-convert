3e3f2ca13aec49eaafd6eedc9a5206f3dd9ad118
Poppo <pop_coe14@hotmail.com>
2017-10-22 23:40:05 +0700

    + Format getlatest.py

--- getLatest.py
+++ getLatest.py
@@ -1,20 +1,24 @@
 import difflib
 
-def show(new):
-	print new
 
-def getLatest(func, oldFile, newFile):
-	d = difflib.Differ()
-	with open(oldFile, 'r') as f1, open(newFile, 'r') as f2:
-		differ = difflib.Differ()
-		new = []
-		for line in differ.compare(f1.read().splitlines() ,f2.read().splitlines()):
-			if line.startswith('+ '):
-				new.append(line[2:])
-	func(new)
+def show(new):
+    print(new)
+
+
+def get_latest(func, oldFile, newFile):
+    d = difflib.Differ()
+    with open(oldFile, 'r') as f1, open(newFile, 'r') as f2:
+        differ = difflib.Differ()
+        new = []
+        for line in differ.compare(f1.read().splitlines() ,f2.read().splitlines()):
+            if line.startswith('+ '):
+                new.append(line[2:])
+    func(new)
+
 
 def main():
-	getLatest(show,'Test.txt.old','Test.txt')
+    get_latest(show, 'Test.txt.old', 'Test.txt')
+
 
 if __name__ == '__main__':
-	main()
+    main()

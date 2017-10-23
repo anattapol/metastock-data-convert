7e845e037258da29e972ebce401e48b821a8820b
Poppo <pop_coe14@hotmail.com>
2017-10-24 02:58:49 +0700

    Fix bash

--- cdcdl.sh
+++ cdcdl.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 [ "$1" == "" ] && WORKDIR=~ || WORKDIR=$1
 
 mkdir -p ${WORKDIR}/ms-data/
 chmod 400 ${WORKDIR}/rsync_pass
 chown jenkins ${WORKDIR}/rsync_pass
-$(which rsync) --password-file ${WORKDIR}/rsync_pass -avz rsync://darthvader@176.32.89.130/msd/SET/ ${WORKDIR}/ms-data/SET/
+$(which rsync) --password-file=${WORKDIR}/rsync_pass -avz rsync://darthvader@176.32.89.130/msd/SET/ ${WORKDIR}/ms-data/SET/

5921bc80cfb634df858ac7eba004cc969ebe9b66
Poppo <pop_coe14@hotmail.com>
2017-10-24 01:58:31 +0700

    Fix cdcdl.sh

--- cdcdl.sh
+++ cdcdl.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-RSYNC=$(which rsync)
-$RSYNC --password-file=~/rsync_pass -avz rsync://darthvader@176.32.89.130/msd/SET/ ~/ms-data/SET/
+
+[ $1 == '' ] && WORKDIR=~ || WORKDIR=$1
+
+mkdir -p ${WORKDIR}/ms-data/
+$(which rsync) --password-file=${WORKDIR}/rsync_pass -avz \
+    rsync://darthvader@176.32.89.130/msd/SET/ ${WORKDIR}/ms-data/SET/

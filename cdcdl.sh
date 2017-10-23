b1ee6ffe8bc523f5fea583215e38700b102f39ef
Poppo <pop_coe14@hotmail.com>
2017-10-24 02:41:35 +0700

    Fix bash

--- cdcdl.sh
+++ cdcdl.sh
@@ -3,4 +3,6 @@
 [ "$1" == "" ] && WORKDIR=~ || WORKDIR=$1
 
 mkdir -p ${WORKDIR}/ms-data/
+cat ${WORKDIR}/rsync_pass
+touch cat ${WORKDIR}/rsync_pass
 $(which rsync) --password-file=${WORKDIR}/rsync_pass -avz rsync://darthvader@176.32.89.130/msd/SET/ ${WORKDIR}/ms-data/SET/
